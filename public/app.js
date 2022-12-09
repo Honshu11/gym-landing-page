51055826daf60b8fda49558ae512dcea3e20bcbc
Honshu11 <egonza67@yahoo.com>
2022-12-09 11:30:39 -0800

    initial setup for personal gym landing page.

---
+++ public/app.js
@@ -0,0 +1,26 @@
+// Get the form element
+const form = document.querySelector('form');
+
+// Create the submit event handler
+const submitHandler = (event) => {
+
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Create an object to hold the form data
+  const formData = {};
+
+  // Get the value of each form element and add it to the formData object
+  formData.name = document.querySelector('#full-name').value;
+  formData.email = document.querySelector('#email').value;
+  
+  //formData.service = document.querySelector('input[name="service"]:checked').value;
+  formData.message = document.querySelector('#message').value;
+  console.log(formData);  
+  // Return the form data
+  return formData;
+};
+
+// Register the submit event handler
+form.addEventListener('submit', submitHandler);
+
